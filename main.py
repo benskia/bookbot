1f3695e31e0abefe4eddc58f60e461c907ec23a5
benskia <benski.abraham@gmail.com>
2024-05-09 19:44:08 -0500

    add charsort and printout

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    FILEPATH = "books/frankenstein.txt"
+    with open(FILEPATH) as f:
         file_contents = f.read()
-        print(count_string_words(file_contents))
-        print(count_letter_occurrences(file_contents))
+        words = count_words(file_contents)
+        character_counts = count_characters(file_contents)
+        sorted_character_counts = sort_character_counts(character_counts)
 
-def count_string_words(text: str) -> int:
+        print(f"--- Begin report of {FILEPATH} ---")
+        print(f"{words} words found in the document\n")
+
+        for ch_ct in sorted_character_counts:
+            print(f"The {ch_ct['character']} character was found {ch_ct['count']} times")
+
+        print("--- End report ---")
+
+def count_words(text: str) -> int:
     words = text.split()
     return len(words)
 
-def count_letter_occurrences(text: str) -> dict:
+def count_characters(text: str) -> dict:
     letter_occurrences = {}
     for ch in text:
+        if not ch.isalpha():
+            continue
         letter = ch.lower()
         if letter not in letter_occurrences:
             letter_occurrences[letter] = 1
             continue
         letter_occurrences[letter] += 1
     return letter_occurrences
+
+def sort_character_counts(char_counts: dict) -> list:
+    sorted_character_counts = []
+    for char, count in char_counts.items():
+        new_char_count = { "character": char, "count": count }
+        sorted_character_counts.append(new_char_count)
+    sorted_character_counts.sort(reverse=True, key=sort_on)
+    return sorted_character_counts
+
+def sort_on(dict: dict) -> int:
+    return dict["count"]
 
 if __name__ == "__main__":
     main()
